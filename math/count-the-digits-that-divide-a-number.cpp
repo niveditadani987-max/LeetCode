bd4e23a203d30a8756c227c22f0812a107854211
niveditadani987-max <niveditadani987@gmail.com>
2026-05-23 12:29:48 +0530

    LC-count-the-digits-that-divide-a-number [Runtime: N/A; Memory: N/A]

---
+++ math/count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count=0;
+        int n=num;
+        while(n>0){
+            int lastdigit=n%10;
+            if (lastdigit != 0 && num % lastdigit == 0){
+                count++;
+            }
+            n=n/10;
+        }
+        return count;
+    }
+};
