fc7a9f2af71e21993243979b2b1176dd77dcbdfa
Nivedita Dani <niveditadani987@gmail.com>
2026-06-03 07:12:58 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+       int prev=x;
+       int rev=0;
+       while(x>0){
+        int ld=x%10;
+        rev=(rev*10)+ld;
+        x/10;
+        if(rev>INT_MAX/10||rev<INT_MIN/10)
+        return false;
+       } 
+       if(prev==rev)
+       return true;
+       else 
+       return false;
     }
 };
