a45870fb181fdb22cf189f10b0019bb5e7ccd06c
Nivedita Dani <niveditadani987@gmail.com>
2026-06-03 08:02:19 +0530

    LC-four-divisors [Runtime: N/A; Memory: N/A]

--- array/four-divisors.cpp
+++ array/four-divisors.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
         int n=nums.size();
-        int count=0;
-        int sum=0;
-        vector<int> ans;
-        for(int i=1;i<=sqrt(n);i++){
-            if(n%i==0){
-            ans.push_back(i);
-            count++;
-            if((n/i)!=i)
-            ans.push_back(n/i);
-            count++;
+        int totalSum=0;
+        for(int n:nums){
+            int count=0;
+            int sum=0;
+            for(int i=1;i<sqrt(n);i++){
+                if(n % i == 0) {
+                    count++;
+                    sum += i;
+
+                    if((n / i) != i) {
+                        count++;
+                        sum += (n / i);
+                    }
+                }
+            }
+            if(count==4){
+            totalSum+=sum;
             }
         }
-    if(count==4){
-        int a=ans.size()-1;
-        for(int i=0;i<a;i++){
-            sum=sum+ans[i];
-        }
-        return sum;
-    }
-    return 0;
+        return totalSum;
     }
 };
