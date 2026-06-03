320408dcbcb9f9af687f40f7959ab288726bd192
Nivedita Dani <niveditadani987@gmail.com>
2026-06-03 07:45:38 +0530

    LC-three-divisors [Runtime: N/A; Memory: N/A]

--- number-theory/three-divisors.cpp
+++ number-theory/three-divisors.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isThree(int n) {
-     if (n == 1) return false;  
-
         int count=0;
-        for(int i=1;i<=sqrt(n);i++){
+        for(int i=1;i<sqrt(n);i++){
             if(n%i==0){
-            count++;
-            if((n/i)!=i)
-            count++;
+                count++;
+                if((n/i)!=i)
+                count++;
+            }
         }
-        }
-        if(count==3)
-        return true;
-        else return false;
+    if(count==3)
+    return true;
+    else
+    return false;
     }
 };
